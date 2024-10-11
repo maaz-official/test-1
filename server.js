173684172ec743a2626c0c83e016090cab494210
Imam Faheem <imamhusain941@gmail.com>
2024-10-11 23:12:44 +0500

    Added routes and middleware files, of basic level

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(bodyParser.json());
 
 connectDB();
 
+app.use('/users', routes.userRoutes);
+app.use('/auth', routes.authRoutes);
+// app.use('/events', routes.eventRoutes);
+// app.use('/players', routes.playerRoutes);
+// app.use('/hosts', routes.hostRoutes);
+// app.use('/map', routes.mapRoutes);
+// app.use('/notifications', routes.notificationRoutes);
+// app.use('/reviews', routes.reviewRoutes);
+// app.use('/leaderboard', routes.leaderboardRoutes);
+// app.use('/analytics', routes.analyticsRoutes);
+// app.use('/files', routes.fileUploadRoutes);
+// app.use('/admin', routes.adminRoutes);
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to the Sports Event API');
 });
